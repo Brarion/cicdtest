2694310bfee85d9bafd222aee42547438e725d55
brarion <pr.nick126@gmail.com>
2023-05-09 18:44:36 +0300

    change start text

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import createServer from './createServer';
 const server = createServer();
 
 server.listen(8000, () => {
-  console.log('server started');
+  console.log('server started after CI/CD');
 });
